7ee177db5797ccee358e48f0a1cbfbc66890ba2a
den0206 <mac.pop@mineo.jp>
2020-10-17 10:10:00 +0900

    Initial Commit

---
+++ five-items/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  five-items
+//
+//  Created by 酒井ゆうき on 2020/10/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ five-items/five_itemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  five_itemsApp.swift
+//  five-items
+//
+//  Created by 酒井ゆうき on 2020/10/17.
+//
+
+import SwiftUI
+
+@main
+struct five_itemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
